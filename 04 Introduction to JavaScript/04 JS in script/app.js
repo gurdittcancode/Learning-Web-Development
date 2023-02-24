0d44628a08416b7e9c20a55d6e6e2e1bd9a70884
Gurditt Singh Khurana <gurdittsingh1508@gmail.com>
2023-02-25 00:33:39 +0530

    First JS script

---
+++ 04 Introduction to JavaScript/04 JS in script/app.js
@@ -0,0 +1,11 @@
+let total = 1 + 3;
+console.log("hello from our first JS file!!");
+console.log("goodbye!");
+
+let x = Math.random();
+
+if(x < 0.5) {
+    console.log("you can drink");
+} else {
+    console.log("no");
+}
