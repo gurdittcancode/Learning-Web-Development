04b7cc7ff41dc2c1202c1d0f16686c7184315595
Gurditt Singh Khurana <gurdittsingh1508@gmail.com>
2023-02-25 22:26:56 +0530

    Continued with arrays in JS

--- 04 Introduction to JavaScript/06 Arrays/app.js
+++ 04 Introduction to JavaScript/06 Arrays/app.js
@@ -3,3 +3,17 @@ let days = ['Monday', 'Tuesday', 'Wednesday'];
 
 let stuff = [true, 68, 'cat', null];
 //MIXED DATA TYPES WTF
+
+let movieLine = ["tom", "nancy"];
+movieLine.push("john");
+let person = movieLine.pop();
+console.log(person);
+
+let barbell = [];
+barbell.push(45);
+barbell.push(45);
+barbell.push(25);
+barbell.push(10);
+barbell.push(2.5);
+barbell.pop();
+
