3e77cae2ae9a318f8e9eda9d2d41c6ab173dbd8c
Gurditt Singh Khurana <gurdittsingh1508@gmail.com>
2023-03-04 10:25:49 +0530

    Continued with DOM

---
+++ 07 DOM Manipulation/01 Selecting/app.js
@@ -0,0 +1,23 @@
+const allImages = document.getElementsByTagName('img')
+
+// for(let img of allImages) {
+//     img.src = 'https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80'
+// }
+
+// const sqImages = document.getElementsByClassName('square')
+// for(let img of sqImages) {
+//     console.log(img.src)
+// }
+
+document.querySelector('#banner');
+document.querySelector('p');
+doucment.querySelector('img:nth-of-type(2)');
+
+//selecting by attribute
+document.querySelector('a[title="Java"]');
+
+document.querySelectorAll('p a');
+
+const links = document.querySelectorAll('a');
+
+// for(let link of links) console.log(link.href);
---
+++ 07 DOM Manipulation/02 Manipulating/app.js
@@ -0,0 +1,24 @@
+// document.querySelector('p').innerText = 'lmfaoooo';
+// const allLinks = document.querySelectorAll('a')
+
+// for(let link of allLinks) {
+//     link.innerText = 'I AM A LINK!!!';
+// }
+
+// document.querySelector('h1').innerText = '<i>LMAOO</i>'; //It will treat it as literal text.
+
+// document.querySelector('h1').innerHTML = '<i>LMAOO</i>'; //It will recognize the <i> tags and will treat them as HTML.
+// document.querySelector('h1').innerHTML += '<sup>lol is this working</sup>'
+
+// document.querySelector('#banner') = 'whoops';
+//document.querySelector('a').href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ'
+//document.querySelector('#banner').src = 'data:image/png;base64,iVBORw0KGgoAAAAN
+
+
+/*
+
+const input = document.querySelector('input[type="text"]');
+input.type = 'password'
+input.type = 'color'
+
+*/
